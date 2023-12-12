281e07e34f06ba448803bd2dc1bd1e7ac518dc50
lionelbui1992 <127284987+lionelbui1992@users.noreply.github.com>
2023-12-12 15:40:50 +0700

    add  test attributes poc

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -34,7 +34,7 @@ export function ProductDescription({ product }: { product: Product }) {
           html={product.descriptionHtml}
         />
       ) : null}
-      <AddToCart variants={product.variants} availableForSale={product.availableForSale} attributes />
+      <AddToCart variants={product.variants} availableForSale={product.availableForSale} attributes=[] />
     </>
   );
 }
