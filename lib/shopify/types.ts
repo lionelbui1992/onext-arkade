9d509a4ae2740dd4a70eef80d096d066db370fe0
lionelbui1992 <127284987+lionelbui1992@users.noreply.github.com>
2023-12-12 09:54:10 +0700

    add  test properties

--- lib/shopify/types.ts
+++ lib/shopify/types.ts
@@ -152,6 +152,7 @@ export type ShopifyAddToCartOperation = {
     lines: {
       merchandiseId: string;
       quantity: number;
+      properties:{}
     }[];
   };
 };
@@ -180,6 +181,7 @@ export type ShopifyUpdateCartOperation = {
       id: string;
       merchandiseId: string;
       quantity: number;
+      properties:{}
     }[];
   };
 };
