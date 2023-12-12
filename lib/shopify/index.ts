7ecebffff18686088db466058235163794ae2e95
lionelbui1992 <127284987+lionelbui1992@users.noreply.github.com>
2023-12-12 10:35:12 +0700

    add  test properties

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -212,7 +212,7 @@ export async function createCart(): Promise<Cart> {
 
 export async function addToCart(
   cartId: string,
-  lines: { merchandiseId: string; quantity: number; properties: { [key: string]: string; } }[]
+  lines: { merchandiseId: string; quantity: number}[]
 ): Promise<Cart> {
   const res = await shopifyFetch<ShopifyAddToCartOperation>({
     query: addToCartMutation,
